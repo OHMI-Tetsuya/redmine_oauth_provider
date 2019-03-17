c5341c6327b3013f750611bc12eecef16d547de0
ayapapa <ayapapajapan@yahoo.co.jp>
2019-03-17 11:31:25 +0900

    ドキュメント更新、アイコン表示エラー対応。

--- init.rb
+++ init.rb
@@ -25,5 +25,6 @@ Redmine::Plugin.register :redmine_oauth_provider do
   url 'https://github.com/Virtualmaster/redmine-oauth-provider'
   author_url 'http://www.jana4u.net/'
 
-  menu :admin_menu, :oauth_clients, { :controller => 'oauth_clients', :action => 'index' }, :caption => :oauth_client_applications
+#  menu :admin_menu, :oauth_clients, { :controller => 'oauth_clients', :action => 'index' }, :caption => :oauth_client_applications
+  menu :admin_menu, 'icon oauth_clients', { :controller => 'oauth_clients', :action => 'index' }, :caption => :oauth_client_applications
 end
